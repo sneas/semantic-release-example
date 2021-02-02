cd6c8487d36a5e03f5715895be5374905ba35edb
Dima Snisarenko <snisarenkodima@gmail.com>
2021-02-02 15:56:37 +0100

    feat: greeting

---
+++ src/greeting.test.js
@@ -0,0 +1,7 @@
+import { greeting } from "./greeting";
+
+describe('greeting', () => {
+  it('correctly outputs hello name string', () => {
+    expect(greeting('world')).toEqual('Hello world')
+  });
+})
