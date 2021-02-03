f9e46df0107d4975ef66b414893ab47204093c01
Dima Snisarenko <snisarenkodima@gmail.com>
2021-02-03 16:21:48 +0100

    feat: greeting with exclamation

--- src/greeting.test.js
+++ src/greeting.test.js
@@ -2,6 +2,6 @@ import { greeting } from "./greeting";
 
 describe('greeting', () => {
   it('correctly outputs hello name string', () => {
-    expect(greeting('world')).toEqual('Hello world')
+    expect(greeting('world')).toEqual('Hello world!')
   });
 })
